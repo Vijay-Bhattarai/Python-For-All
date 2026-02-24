b69e73e55a33fc1ef76cd65c63f105a0ca65dc46
unknown <bijaybhattrarai5544@gmail.com>
2026-02-24 19:30:14 +0545

    initial commit

---
+++ index.py
@@ -0,0 +1,61 @@
+
+# #this are strings
+# first_name = "Bhattarai"
+# food = "I like Pizza"
+# email= "bijaybhattrarai5544@gmail.com"
+
+# print(f"Vijay {first_name}")
+# print(f"Hey Man, {food}")
+# print(f"hey its my email you can contact directly from here- {email}")
+
+
+# #Integers 
+# age = 26
+
+# print(f"you are {age} years old.")
+
+# #float 
+# price = 7.99 
+
+# print(f"the price is {price}")
+
+
+# #Boolean
+
+# is_student = True
+
+
+# # #TypeCasting
+# # person_name = "vijay"
+# # age = 26
+# # gpa = 3.40
+# # is_student = True
+
+
+# # person_name = bool(person_name)
+
+# # print(person_name)
+
+# # #input 
+# # person_name= input("What is your name: ")
+# # age = int(input("How old are you ?: "))
+
+# # # age = int(age)
+# # age = age + 1 
+
+# # print(f"its good name {person_name}")
+# # print(" Happy Birthday!")
+# # print(f"Your are {age} Years old.")
+ 
+#Calucate 
+
+Length = float(input(" Enter the length: "))
+Width = float(input(" Enter the Width: "))
+
+Area = Length * Width 
+
+print( f" The Area is : {Area} cm ") 
+
+#Shopping Cart Program 
+
+item = input()
