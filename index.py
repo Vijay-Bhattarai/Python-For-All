33066e6c0ce29b1683d133b99c16d6bd99ce5aa1
unknown <bijaybhattrarai5544@gmail.com>
2026-02-25 02:06:24 +0545

    Good Night Everyone

--- index.py
+++ index.py
@@ -71,14 +71,140 @@
 # Word Game where you create a story 
 # By filling in blanks with random words
 
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing':")
-adjective3 = input("Enter an adjective (description): ")
+# adjective1 = input("Enter an adjective (description): ")
+# noun1 = input("Enter a noun (person, place, thing): ")
+# adjective2 = input("Enter an adjective (description): ")
+# verb1 = input("Enter a verb ending with 'ing':")
+# adjective3 = input("Enter an adjective (description): ")
 
 
-print(f"Today i went to a {adjective1} zoo.")
-print(f" In an exhibit, I saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
+# print(f"Today i went to a {adjective1} zoo.")
+# print(f" In an exhibit, I saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}!")
+
+
+# Arthmetic Operators
+# friends = 10
+# friends = friends + 1 
+# friends += 1
+# friends = friends - 1
+# friends -= 1
+# friends  = friends * 3
+# friends *= 3
+# friends = friends / 2
+
+# friends /= 2
+
+# friends = friends ** 2
+# friends **=2
+
+# remainder = friends % 3
+
+# print(remainder)
+
+
+# Math Function 
+
+# x = 3.14
+# y = 2
+# z= 5
+
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+
+# print(result)
+
+
+# import math
+
+# x = 9.9
+
+# # print(math.pi)
+# # print(math.e)
+
+# # result = math.sqrt(x)
+# # result = math.ceil(x)
+# result = math.floor(x)
+
+# print(result)
+
+# import math
+
+
+# radius = float(input('Enter the radius of a circle: '))
+
+
+# circumference = 2 * math.pi * radius
+# area = math.pi * radius ** 2
+
+# print(f"The circumference is: {round(circumference, 2)}cm")
+ 
+
+# if - Do some code only if some condtion is True 
+#     Else do something else 
+
+
+# age = int(input("Enter your age: "))
+
+# if age >= 120:
+#     print("You cannot vote!")
+# elif age >= 18:
+#     print("You are old enough to vote!")
+# elif age < 0:
+#     print("Please enter a valid age!")
+
+# else:
+#     print("You are too young to vote!")
+
+
+# response = input("Do you like Python? (yes/no):")
+
+# if response == "yes":
+#     print("Great! I love Python too!")
+# else:
+#     print("Okay, maybe another time.")
+
+# name = input("Enter your name:")
+
+# if name == "":
+#     print("You did not type in your name!")
+# else:
+#     print(f"Hello {name}")
+
+# for_sale = False
+
+# if for_sale:
+#     print("Item is for sale!")
+# else:
+#     print( "Item is not for sale!")
+
+# # Python calculator
+# operator = input("Enter an operator (+, -, *, /): ")
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter Second number: "))
+
+# if operator == "+":
+#     result = num1 + num2
+#     print (round(result, 3))
+# elif operator == "-":
+#     result = num1 - num2
+#     print (round(result, 3))
+# elif operator == "*":
+#     result = num1 * num2
+#     print (round(result, 3))
+# elif operator == "/":
+#     result = num1 / num2
+#     print (round(result, 3)) 
+# else:
+#     print(f"{operator} is not a valid operator")
+
+
+ # Python weight conveter Program 
+
+    
